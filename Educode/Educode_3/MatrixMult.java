e71f3223b0d9d196253722ed9f755f8d56e4d59e
Nessaiy <martinkuhlmann@hotmail.de>
2020-12-14 08:02:16 +0100

    .

--- Educode/Educode_3/MatrixMult.java
+++ Educode/Educode_3/MatrixMult.java
@@ -17,52 +17,63 @@ public class MatrixMult {
             offset = offset + 2; //Um beim ersten Matrix wert zu starten + 2
             for(int i = 0; i < zeilen; i++){
                 for(int j = 0; j < spalten; j++){
-                    int inputZahl = Integer.parseInt(input[offset]);
-                    matrix[i][j] = inputZahl;
-                    offset = offset + 1;
+                    matrix[i][j] = Integer.parseInt(input[offset]);
+                    offset = offset + 1; //einen wert wandern
                 }
             }
         }
-
-        return matrix;
-
+        return matrix; //Matrix zurückgeben
     }
 
     private static int computeEntry(int[][] m1, int[][] m2, int row, int column) {
         int computeEntry = 0;
         
-        for (int i = 0; i < m2[0].length; i++){
+        for (int i = 0; i < m1[0].length; i++){
             computeEntry += m1[row][i] * m2[i][column]; 
         }
-
         return computeEntry;
-    
     }
 
     public static void main(String[] args) {
-        int row = Integer.parseInt(args[0]);
-        int column = Integer.parseInt(args[1]);
+        int row1 = Integer.parseInt(args[0]);
+        int column1 = Integer.parseInt(args[1]);
+
+        int offset = row1 * column1 + 2;
+
+        int row2 = Integer.parseInt(args[offset]);
+        int column2 = Integer.parseInt(args[offset + 1]);
+
+        
+
+        if (column1 != row2){
+            System.out.println("ERROR");
+            return;
+        }
 
         //Matrix 1 einlesen
-        int[][] matrix_1 = readMatrix(args, 0);
+        int[][] matrix1 = readMatrix(args, 0);
 
         //Matrix 2 einlesen
-        int[][] matrix_2 = readMatrix(args, row * column + 2);
+        int[][] matrix2 = readMatrix(args, offset);
 
+        
         //MatrixMult berechnen
-        int[][] matrix_produkt = new int [row][column];
+        int[][] matrixProdukt = new int [row1][column2];
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
-                matrix_produkt[i][j] = computeEntry(matrix_1, matrix_2, i, j);
+        for(int i = 0; i < row1; i++){
+            for(int j = 0; j < column2; j++){
+                matrixProdukt[i][j] = computeEntry(matrix1, matrix2, i, j);
             }
         }
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < column; j++){
-                System.out.print(matrix_produkt[i][j]);
+        for(int i = 0; i < row1; i++){
+            for(int j = 0; j < column2; j++){
+                System.out.print(matrixProdukt[i][j]+ " ");
             }
             System.out.println();
         }
     }
 }
+//"2", "4", "1", "2", "7", "8", "2", "6", "5", "3", "4", "3", "2", "8", "9", "1", "12", "15", "42", "3", "6", "50", "21", "4"
+// r1   c1   0,0  0,1  0,2  0,3  1,0  1,1  1,2  1,3  r2  c2  0,0  0,1   0,2  0,3  1,0   1,1   1,2   1,3   2,0  2,1   2,2   2,3
+// 0    1    2    3    4    5    6    7    8    9    10  11  12   13    14   15   16    17    18    19    20   21    22    24
